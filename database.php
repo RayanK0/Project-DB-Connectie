7ccf8f89a281fe54d88d82a5c652ff37783648e1
Rayan <59512147+RayanK0@users.noreply.github.com>
2023-05-25 20:46:56 +0200

    Update database.php

--- database.php
+++ database.php
@@ -4,14 +4,14 @@
 
 $server = "localhost";
 $gebruiker = "root";
-$wachtwoord = "rayan";
+$wachtwoord = "";
 $databasenaam = "keukenprins";
 
 $dbVerbinding = new mysqli ($server, $gebruiker, $wachtwoord, $databasenaam);
 
 if ($dbVerbinding -> connect_errno) 
 {
-    echo "Failed to connect to MySQL: " . $dbVerbinding -> connect_error;
+    echo "Niet gelukt om te verbinden: " . $dbVerbinding -> connect_error;
     exit();
 }
 
